4bf4d264e56c29908d278941a7750247082eb199
Krutartha Nagesh <krutartha2002@gmail.com>
2025-05-29 18:26:17 -0400

    fix: update Analytics component to production mode

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-        <Analytics />
+        <Analytics mode="production" />
       </body>
     </html>
   )
